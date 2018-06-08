7df3cf72d5e8514e0e1bf66a71785e89ee4f565d
wanggaowu <wanggaowu@qq.com>
2018-06-08 13:10:39 +0800

    // edit

--- app/src/main/java/com/unistrong/demo/MainActivity.java
+++ app/src/main/java/com/unistrong/demo/MainActivity.java
@@ -1,15 +1,19 @@
 package com.unistrong.demo;
 
+import android.content.Intent;
 import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.picture.lib.PictureSelector;
+import com.picture.lib.config.PictureConfig;
+import com.picture.lib.config.PictureMimeType;
+import com.picture.lib.entity.LocalMedia;
 import com.unistrong.baselibs.style.BaseActivity;
-import com.unistrong.requestlibs.request.HttpRequestImpl;
-import com.unistrong.requestlibs.response.ResponseBody;
 
-import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
@@ -25,6 +29,7 @@ public class MainActivity extends BaseActivity {
     }
 
     public void request(View view) {
+        //请求网址
 //        String imagePath = "http://www.xinhuanet.com/world/2015-12/03/128496492_14491425659751n.jpg";
 //        Picasso.get().load(imagePath).into(iv);
 //        String serverUrl = "http://issuecdn.baidupcs.com/issue/netdisk/yunguanjia/BaiduNetdisk_6.2.0.exe";
@@ -49,24 +54,45 @@ public class MainActivity extends BaseActivity {
 //            }
 //        });
 
-        String url = "https://www.sojson.com/open/api/weather/json.shtml";
-        HashMap<String, String> map = new HashMap<>();
-        map.put("city", "北京");
-        HttpRequestImpl.getInstance().requestGet(url, map, new ResponseBody<Weather>(Weather.class) {
-            @Override
-            public void onSuccess(Weather weather) {
-                //     viewById.setText(weather.toString());
-            }
+        //请求
+//        String url = "https://www.sojson.com/open/api/weather/json.shtml";
+//        HashMap<String, String> map = new HashMap<>();
+//        map.put("city", "北京");
+//        HttpRequestImpl.getInstance().requestGet(url, map, new ResponseBody<Weather>(Weather.class) {
+//            @Override
+//            public void onSuccess(Weather weather) {
+//                //     viewById.setText(weather.toString());
+//            }
+//
+//            @Override
+//            public void onSuccess(String json) {
+//                viewById.setText(json);
+//            }
+//
+//            @Override
+//            public void onFailure(String message) {
+//                viewById.setText("null!");
+//            }
+//        });
 
-            @Override
-            public void onSuccess(String json) {
-                viewById.setText(json);
-            }
-
-            @Override
-            public void onFailure(String message) {
-                viewById.setText("null!");
-            }
-        });
+        //选择照片
+        PictureSelector.create(this).openGallery(PictureMimeType.ofImage())
+                .forResult(PictureConfig.CHOOSE_REQUEST);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PictureConfig.CHOOSE_REQUEST:
+                    // 图片、视频、音频选择结果回调
+                    List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+                    String imagePath = selectList.get(0).getPath();
+                    Glide.with(MainActivity.this).load(imagePath).into(iv);
+                    break;
+            }
+        }
+    }
+
 }
